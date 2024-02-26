1bd0a664549e03836b656cc1958391506c1f4842
LMDev024 <luismalagondev@gmail.com>
2024-02-26 12:03:02 -0500

    Login Form HTML and base STYLE / ADD

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.5)',
+        '4xl': [
+          '0 35px 35px rgba(0, 0, 0, 0.5)',
+          '0 45px 65px rgba(0, 0, 0, 0.3)'
+        ]
+      }
+    },
+
   },
   plugins: [],
 }
